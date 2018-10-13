9782a4ab5c00795decaa4bf47367ce6fa94381a8
Voytin Evgeny <vexelb@gmail.com>
2018-10-13 14:37:48 +0300

    Update rgb.js

--- rgb.js
+++ rgb.js
@@ -5,3 +5,14 @@ function rgb(r = 255, g = 255, b = 255) {
 }
 
 console.log(rgb(14, 188));
+  
+document.querySelector('button')
+.addEventListener(
+  'click',
+   e => {
+     // здесь код вызова функции rgb
+     const result = rgb(255,10);
+     document.body.style.backgroundColor = result;
+     e.target.textContent = result; // вывод результата 
+   }
+);
